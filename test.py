31c8ebcf35f6b6604182710b9a143bf92d57f3f6
zardatleo <z2402519120@gmail.com>
2024-09-19 16:25:40 +0800

    dataset sampling

--- test.py
+++
@@ -1,4 +0,0 @@
-link = "http://paper.people.com.cn/rmrb/html/2024-08/01/nbs.D110000renmrb_01.htm"
-art_link = "http://paper.people.com.cn/rmrb/html/2024-08/01/nw.D110000renmrb_20240801_1-01.htm"
-for i in range(len(art_link)):
-    print(i, art_link[i])
