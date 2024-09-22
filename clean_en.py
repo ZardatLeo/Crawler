e486e1738ce3d3e38244a17fc26375603ffedeb8
zardatleo <z2402519120@gmail.com>
2024-09-22 15:06:30 +0800

    dataset clean

--- clean_en.py
+++ clean_en.py
@@ -2,27 +2,30 @@ import string
 import re
 from nltk.corpus import stopwords
 
-with open("output_en.txt", "r", encoding="utf-8") as file:
-    data = file.read()
+for i in range(1, 5):
+    file_path = './' + str(i * 2) + 'MB/' + str(i * 2) + 'MB_en.txt'
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = file.read()
 
-# 去除制表符和换行符，但保留文本中的空格
-data = [line.rstrip('\n\r\t') for line in data if line.rstrip('\n\r\t')]
+    # 去除制表符和换行符，但保留文本中的空格
+    data = [line.rstrip('\n\r\t') for line in data if line.rstrip('\n\r\t')]
 
-# 移除英文标点符号
-data = ''.join([char for char in data if char not in string.punctuation])
-# 将所有英文字符转为小写
-data = data.lower()
+    # 移除英文标点符号
+    data = ''.join([char for char in data if char not in string.punctuation])
+    # 将所有英文字符转为小写
+    data = data.lower()
 
-# 由于要验证齐夫定律，因此不进行停用词清理
-# # 载入英文停用词
-# stop_words = set(stopwords.words('english'))
-# data = ' '.join([word for word in data.split() if word not in stop_words])
+    # 由于要验证齐夫定律，因此不进行停用词清理
+    # # 载入英文停用词
+    # stop_words = set(stopwords.words('english'))
+    # data = ' '.join([word for word in data.split() if word not in stop_words])
 
-# 删除数字
-data = re.sub(r'\d+', '', data)
+    # 删除数字
+    data = re.sub(r'\d+', '', data)
 
-# 删除网址
-data = re.sub(r'http[s]?://\S+|www\.\S+', '', data)
+    # 删除网址
+    data = re.sub(r'http[s]?://\S+|www\.\S+', '', data)
 
-with open("chean_en.txt", "w", encoding="utf-8") as file:
-    file.write(data)
+    output_path = './' + str(i * 2) + 'MB/' + str(i * 2) + 'MB_en_clean.txt'
+    with open(output_path, "w", encoding="utf-8") as file:
+        file.write(data)
--- clean_zh.py
+++ clean_zh.py
@@ -1,19 +1,25 @@
 import re
+import string
 # 将文件转换为 UTF-8 编码
-with open('output_zh.txt', 'r', encoding='utf-8') as file:
-    data = file.read()
+for i in range(1, 5): 
+    file_path = './' + str(i * 2) + 'MB/' + str(i * 2) + 'MB_zh.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = file.read()
 
-# 去除空白字符、制表符、换行符等
-data = [line.strip() for line in data if line.strip()]
+    # 去除空白字符、制表符、换行符等
+    data = [line.strip() for line in data if line.strip()]
+    data_str = ''.join(data)  # 用换行符连接每一行
+    data = data_str
+    # 使用正则表达式移除中文标点符号
+    data = re.sub(r'[，。！？、（）《》：“”；]', '', data)
+    # 移除英文标点符号
+    data = ''.join([char for char in data if char not in string.punctuation])
+    # 删除数字
+    data = re.sub(r'\d+', '', data)
 
-# 使用正则表达式移除中文标点符号
-data = re.sub(r'[，。！？、（）《》：“”；]', '', data)
+    # 删除网址
+    data = re.sub(r'http[s]?://\S+|www\.\S+', '', data)
 
-# 删除数字
-data = re.sub(r'\d+', '', data)
-
-# 删除网址
-data = re.sub(r'http\S+|www\S+', '', data)
-
-with open('clean_zh.txt', 'w', encoding='utf-8') as file:
-    file.write(data)
+    output_path = './' + str(i * 2) + 'MB/' + str(i * 2) + 'MB_zh_clean.txt'
+    with open(output_path, 'w', encoding='utf-8') as file:
+        file.write(data)
